c3793b793d037f460f8da0f5abde90cc12859e5f
Jake Walsh <jkwalsh127@gmail.com>
2022-04-07 21:17:59 -0700

    add function to append weather icon for select location

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,49 +10,46 @@ function getGeoCoordinates() {
         })
         .then(function (data) {
             
-            var cityName = document.createElement('h3');
+            // set variables to carry integer values
             var lon = data[0].lon;
             var lat = data[0].lat;
 
-            cityName.textContent = data;
-            console.log(lat);
-            console.log(lon);
             localStorage.setItem("lat", lat);
             localStorage.setItem("lon", lon);
 
             currentDayContainter.append("lat: " + lat);
             currentDayContainter.append("lon: " + lon);
-            
-         })
+            if (lat !== null) {
+                getWeather();
+            }
+    })
 };
 
 
 getGeoCoordinates();
 
+
 function getWeather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=50508f792fb6351a821be7b026bc4041';
+    // receive lat and lon inputs as strings
+    var lat = localStorage.getItem("lat");
+    var lon = localStorage.getItem("lon");
+    // concatenate lat and lon into the requestUrl to obtain the weather data for that location
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=50508f792fb6351a821be7b026bc4041';
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-                // var cityName = document.createElement('h3');
-                // var temperature = document.createElement('h4');
-                // var humidity = document.createElement('h4'); 
-                // var windSpeed = document.createElement('h4'); 
-                var uvIndex = document.createElement('h4'); 
 
-                // cityName.textContent = data[i].name;
-                // temperature.textContent = data[i].name;
-                // humidity.textContent = data[i].name;
-                // windSpeed.textContent = data[i].name;
-                uvIndex.textContent = data;
+                console.log(data.wind.speed);
+                // set variable to carry the 'icon' key value representative of the weather logo as a string
+                var iconKey = data.weather[0].icon;
+                var iconUrl = "http://openweathermap.org/img/w/" + iconKey + ".png";
+                // assign url as source to img element
+                $('#weathericon').attr('src', iconUrl);
 
-                console.log(data.coord.lat);
-                console.log(data.coord.lon);
-
-                // currentDayContainter.append(uvIndex);
             });
 };
 
-getWeather();
+
+
