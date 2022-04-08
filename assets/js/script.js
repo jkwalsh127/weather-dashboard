3b256a68ded66e559b536541988750cff8938b1b
Jake Walsh <jkwalsh127@gmail.com>
2022-04-07 22:29:46 -0700

    add function to take user search and perform API query for the desired city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
 var currentDayContainter = document.getElementById('currentDayContainer');
-
+var searchInput = document.getElementById('searchInput');
+var searchBtn = document.getElementById('searchBtn');
 // var cityName = 
 
+searchBtn.addEventListener("click", getGeoCoordinates);
+
+
+// function grabCity() {
+//     localStorage.setItem("userInput", userInput);
+//     console.log(userInput);
+//     getGeoCoordinates();
+// };
 function getGeoCoordinates() {
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=50508f792fb6351a821be7b026bc4041';
+    var userInput = searchInput.value;
+    console.log(userInput);
+
+    // var cityName = localStorage.getItem("userInput");
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=50508f792fb6351a821be7b026bc4041';
+    // clear user input from input element
+    searchInput.value = null;
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            
+            console.log(data);
             // set variables to carry integer values
             var lon = data[0].lon;
             var lat = data[0].lat;
@@ -26,7 +41,7 @@ function getGeoCoordinates() {
 };
 
 
-getGeoCoordinates();
+// getGeoCoordinates();
 
 
 function getWeather() {
@@ -40,14 +55,23 @@ function getWeather() {
             return response.json();
         })
         .then(function (data) {
-
+                console.log(data);
                 console.log(data.wind.speed);
                 // set variable to carry the 'icon' key value representative of the weather logo as a string
                 var iconKey = data.weather[0].icon;
                 var iconUrl = "http://openweathermap.org/img/w/" + iconKey + ".png";
                 // assign url as source to img element
                 $('#weathericon').attr('src', iconUrl);
+                var cityName = data.name;
+                console.log(cityName);
+                var temp = data.main.temp;
+                console.log(temp);
+                var humidity = data.main.humidity;
+                console.log(humidity);
 
+                currentDayContainter.append(cityName);
+                currentDayContainter.append(temp);
+                currentDayContainter.append(humidity);
             });
 };
 
