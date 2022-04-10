2dc5c7be2219fbd3764d09363ade1d97cf304787
Jake Walsh <jkwalsh127@gmail.com>
2022-04-10 11:40:49 -0700

    add functionality to display current temp, humidity, and windspeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-var currentDayContainter = document.getElementById('currentDayContainer');
+var key = config.OPEN_WEATHER_KEY;
+
+var currentDayContainter = document.getElementById('currentDayHeader');
+var cityNameHeader = document.getElementById('cityNameHeader');
 var searchInput = document.getElementById('searchInput');
 var searchBtn = document.getElementById('searchBtn');
-// var cityName = 
+var currentTemp = $('#currentTemp');
+var currentHumidity = $('#currentHumidity');
+var currentWindspeed = $('#currentWindspeed');
+var currentUV = $('#currentUV');
+
+var cityName = '';
+var temp = '';
+var humidity = '';
+var windspeed = '';
 
 searchBtn.addEventListener("click", getGeoCoordinates);
 
@@ -16,7 +27,7 @@ function getGeoCoordinates() {
     console.log(userInput);
 
     // var cityName = localStorage.getItem("userInput");
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=847bed9dfc4d0e0da832e4cfefa1ef06';
+    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=' + key;
     // clear user input from input element
     searchInput.value = null;
     fetch(requestUrl)
@@ -32,8 +43,6 @@ function getGeoCoordinates() {
             localStorage.setItem("lat", lat);
             localStorage.setItem("lon", lon);
 
-            currentDayContainter.append("lat: " + lat);
-            currentDayContainter.append("lon: " + lon);
             if (lat !== null) {
                 getWeather();
             }
@@ -45,33 +54,51 @@ function getGeoCoordinates() {
 
 
 function getWeather() {
+    // cityName.innerHTML = '';
+    // temp.innerHTML = '';
+    // humidity.innerHTML = '';
+    // windspeed.innerHTML = '';
+    // cityNameHeader.innerHTML = '';
+    // currentTemp.append(temp);
+    // currentHumidity.append(humidity);
+    // currentWindspeed.append(windspeed);
+    // console.log(temp);
+
+
     // receive lat and lon inputs as strings
     var lat = localStorage.getItem("lat");
     var lon = localStorage.getItem("lon");
     // concatenate lat and lon into the requestUrl to obtain the weather data for that location
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=847bed9dfc4d0e0da832e4cfefa1ef06';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key;
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
                 console.log(data);
-                console.log(data.wind.speed);
-                // set variable to carry the 'icon' key value representative of the weather logo as a string
+                // set variable to carry the 'icon' key value representative of the weather logo
                 var iconKey = data.weather[0].icon;
                 var iconUrl = "http://openweathermap.org/img/w/" + iconKey + ".png";
                 // assign url as source to img element
                 $('#weathericon').attr('src', iconUrl);
-                var cityName = data.name;
-                console.log(cityName);
-                var temp = data.main.temp;
-                console.log(temp);
-                var humidity = data.main.humidity;
-                console.log(humidity);
 
-                currentDayContainter.append(cityName);
-                currentDayContainter.append(temp);
-                currentDayContainter.append(humidity);
+
+
+                cityName = data.name;
+                temp =  ' ' + parseInt(((data.main.temp - 273.15) * (9/5) + 32)) + ' °F';
+                humidity = ' ' + data.main.humidity + '%';
+                windspeed = ' ' + data.wind.speed + ' MPH';
+                console.log(parseInt(temp));
+                console.log(Math.floor(temp));
+                tempRounded = Math.floor(parseInt(temp));
+                console.log(tempRounded);
+
+
+                cityNameHeader.innerHTML = cityName;
+
+                $('#currentTemp').text(temp);
+                $('#currentHumidity').text(humidity);
+                $('#currentWindspeed').text(windspeed);
             });
 };
 
@@ -82,7 +109,7 @@ function getForecast() {
     var lat = localStorage.getItem("lat");
     var lon = localStorage.getItem("lon");
     // concatenate lat and lon into the requestUrl to obtain the weather data for that location
-    var requestUrl = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + lat + '&lon=' + lon + '&appid=847bed9dfc4d0e0da832e4cfefa1ef06';
+    var requestUrl = 'https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=' + lat + '&lon=' + lon + '&appid=' + key;
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
